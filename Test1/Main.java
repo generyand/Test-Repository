a0c68872e212bd913a1ff357bbc279f896a960d6
generyand <generyanaceron@gmail.com>
2023-04-06 15:59:11 +0800

    Test 1

---
+++ Test1/Main.java
@@ -0,0 +1,15 @@
+package Test1;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int randNum = rand.nextInt(9) + 1;
+        System.out.println(randNum);
+        
+    }
+    
+}
